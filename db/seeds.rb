e5e41a88017932eebe373d03a0ff5bca692b5127
fumihumi <t.fum1hum@gmail.com>
2019-05-17 03:33:10 +0900

    create User model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+%w[users].each { |table_name| require_relative "./seeds/#{table_name}.rb" }
