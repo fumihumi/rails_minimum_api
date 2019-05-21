52d048b52d34d6d32a31d265c4f97a7a2740d577
fumihumi <t.fum1hum@gmail.com>
2019-05-21 09:15:40 +0900

    register

--- app/controllers/api/authentication_controller.rb
+++ app/controllers/api/authentication_controller.rb
@@ -2,7 +2,7 @@
 
 module Api
   class AuthenticationController < Api::ApplicationController
-    before_action :authorize, except: :login
+    before_action :authorize, except: %i[login register]
 
     def login
       user = User.find_by(email: login_params[:email])
@@ -17,10 +17,34 @@ module Api
       end
     end
 
+    def register
+      user = User.new(register_params)
+
+      if user.save
+        token = JsonWebToken.encode(user_id: user.id)
+        time = Time.now + 24.hours.to_i
+
+        render json: { token: token, expired_at: time.to_s }, status: :ok
+      else
+        render json: {
+          error: user.errors.full_messages
+        }, status: 400
+      end
+    end
+
     private
 
     def login_params
       params.permit(:email, :password)
+    end
+
+    def register_params
+      params.permit(
+        :name,
+        :nickname,
+        :email,
+        :password
+      )
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :todos
+  validates :email, uniqueness: true , presence: true
+  validates :nickname, presence: true
+  validates :name, presence: true
+  validates :password, presence: true
 end
